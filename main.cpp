dd1492fa4139483946a667c9e88aff6f444fe9e7
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-05-12 23:46:12 +0200

    none Corrected program

--- main.cpp
+++ main.cpp
@@ -6,85 +6,95 @@
 
 using json = nlohmann::json;
 
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) {
-    size_t newLength = size * nmemb;
-    size_t oldLength = s->size();
-    try {
-        s->resize(oldLength + newLength);
-    }
-    catch (std::bad_alloc& e) {
-        return 0;
-    }
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) {
+	size_t newLength = size * nmemb;
+	size_t oldLength = s->size();
+	try {
+		s->resize(oldLength + newLength);
+	}
+	catch (std::bad_alloc& e) {
+		std::cerr << "Memory allocation failed: " << e.what() << std::endl;
+		return 0;
+	}
 
-    std::copy((char*)contents, (char*)contents + newLength, s->begin() + oldLength);
-    return size * nmemb;
+	std::copy((char*)contents, (char*)contents + newLength, s->begin() + oldLength);
+	return size * nmemb;
 }
 
 std::string getJSON(std::string url) {
-    CURL* curl;
-    CURLcode res;
-    std::string readBuffer;
+	CURL* curl;
+	CURLcode res;
+	std::string readBuffer;
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
-    return readBuffer;
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+	return readBuffer;
 }
 
 int main(int argc, char* argv[]) {
-    std::string uri = "http://localhost:8384/ranges";
+	std::string uri;
+	int indexOffset = 0;
 
-    if (argc == 4) {
-        int start = std::stoi(argv[1]);
-        int lunchStart = std::stoi(argv[2]);
-        int minutesOfLunchBreak = std::stoi(argv[3]);
-        uri += "WithStartLunchAndMinutesOfLunchBreak/" + std::to_string(start) + "/" + std::to_string(lunchStart) + "/" + std::to_string(minutesOfLunchBreak);
-    }
-    else if (argc == 2) {
-        int minutesOfLunchBreak = std::stoi(argv[1]);
-        uri += "/" + std::to_string(minutesOfLunchBreak);
-    }
-    else {
-        uri += "/30";
-    }
+	if (argc >= 2 && std::strstr(argv[1], "http") != nullptr) {
+		uri = argv[1];
+		indexOffset = 1;
+	}
+	else {
+		uri = "http://localhost:8384/ranges"; //Default to local host
+	}
 
-    std::cout << uri << std::endl;
-    std::string response = getJSON(uri);
-    auto json = json::parse(response);
+	if (argc >= (4 + indexOffset)) {
+		int start = std::stoi(argv[1 + indexOffset]);
+		int lunchStart = std::stoi(argv[2 + indexOffset]);
+		int minutesOfLunchBreak = std::stoi(argv[3 + indexOffset]);
+		uri += "WithStartLunchAndMinutesOfLunchBreak/" + std::to_string(start) + "/" + std::to_string(lunchStart) + "/" + std::to_string(minutesOfLunchBreak);
+	}
+	else if (argc >= (2 + indexOffset)) {
+		int minutesOfLunchBreak = std::stoi(argv[1 + indexOffset]);
+		uri += "/" + std::to_string(minutesOfLunchBreak);
+	}
+	else {
+		uri += "/30";
+	}
 
-    if (json == nullptr) {
-        std::cout << "No data found" << std::endl;
-        return 0;
-    }
+	std::cout << uri << std::endl;
+	std::string response = getJSON(uri);
+	auto json = json::parse(response);
 
-    auto& rangeDetails = json["rangeDetails"];
+	if (json == nullptr) {
+		std::cout << "No data found" << std::endl;
+		return 0;
+	}
 
-    if (rangeDetails == nullptr) {
-        std::cout << "No data found" << std::endl;
-        return 0;
-    }
+	auto& rangeDetails = json["rangeDetails"];
 
-    std::cout << std::setw(0) << "start" << std::setw(8) << "end" << std::setw(18) << "duration" << std::setw(18) << "durationInHours" << std::endl;
-    std::cout << std::string(49, '-') << std::endl;
+	if (rangeDetails == nullptr) {
+		std::cout << "No data found" << std::endl;
+		return 0;
+	}
 
-    for (auto& rangeDetail : rangeDetails) {
-        if (rangeDetail != nullptr && rangeDetail["range"] != nullptr) {
-            std::cout << std::setw(0) << rangeDetail["range"]["start"].get<std::string>() << std::setw(10) << rangeDetail["range"]["end"].get<std::string>() << std::setw(10) << rangeDetail["duration"].get<std::string>() << std::setw(10) << rangeDetail["durationInHours"].get<std::string>() << std::endl;
-        }
-    }
-    std::cout << std::endl;
+	std::cout << std::setw(0) << "start" << std::setw(8) << "end" << std::setw(18) << "duration" << std::setw(18) << "durationInHours" << std::endl;
+	std::cout << std::string(49, '-') << std::endl;
 
-    std::cout << std::setw(0) << "totalHours" << std::setw(20) << "totalHoursInHHMM" << std::setw(27) << "expectedLunchTimeInHHMM" << std::endl;
-    std::cout << std::string(60, '-') << std::endl;
-    std::cout << std::setw(0) << json["totalHours"].get<std::string>() << std::setw(15) << json["totalHoursInHHMM"].get<std::string>() << std::setw(20) << json["expectedLunchTimeInHHMM"].get<std::string>() << std::endl;
+	for (auto& rangeDetail : rangeDetails) {
+		if (rangeDetail != nullptr && rangeDetail["range"] != nullptr) {
+			std::cout << std::setw(0) << rangeDetail["range"]["start"].get<std::string>() << std::setw(10) << rangeDetail["range"]["end"].get<std::string>() << std::setw(10) << rangeDetail["duration"].get<std::string>() << std::setw(10) << rangeDetail["durationInHours"].get<std::string>() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+
+	std::cout << std::setw(0) << "totalHours" << std::setw(20) << "totalHoursInHHMM" << std::setw(27) << "expectedLunchTimeInHHMM" << std::endl;
+	std::cout << std::string(60, '-') << std::endl;
+	std::cout << std::setw(0) << json["totalHours"].get<std::string>() << std::setw(15) << json["totalHoursInHHMM"].get<std::string>() << std::setw(20) << json["expectedLunchTimeInHHMM"].get<std::string>() << std::endl;
 
 
-    return 0;
+	return 0;
 }
